50d82ee108ab7ca1f65deff7513b4703bbb946c2
Jacek <jacek.poreda@sonarsource.com>
2025-07-30 11:52:12 +0200

    Update auth.js

--- model/auth.js
+++ model/auth.js
@@ -5,23 +5,7 @@ var config = require("../config"),
 function do_auth(username, password) {
     var test = "Unused"
 
-    var test1 = "Unused"
-
-    var test2 = "Unused"
-
-    var test3 = "Unused"
-
-    var test4 = "Unused"
-
-    var test5 = "Unused"
-
-    var test6 = "Unused"
-
-    var test7 = "Unused"
-
-    var test8 = "Unused"
-
-    var test9 = "Unused"
+    var test99 = "Unused"
     
     var db = pgp(config.db.connectionString);
 
