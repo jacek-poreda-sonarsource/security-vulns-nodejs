9e650ca7664c4bf9fc44c5b1b4e8f54ef35768b6
Jacek <jacek.poreda@sonarsource.com>
2025-09-12 11:26:15 +0200

    Update auth.js

--- model/auth.js
+++ model/auth.js
@@ -5,6 +5,8 @@ var config = require("../config"),
 function do_auth(username, password) {
     var db = pgp(config.db.connectionString);
 
+    var etsdtsdt = " unused"
+    
     var q = "SELECT * FROM users WHERE name = '" + username + "' AND password ='" + password + "';";
 
     return db.one(q);
